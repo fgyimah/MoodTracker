1b568eb292de2b15b2ce7b8e43731ae8f8daf0cc
Francis Gyimah <fgyimah883@gmail.com>
2021-12-31 19:39:07 +0000

    feat: persisted mood list over application restarts

--- src/providers/moodLists.provider.tsx
+++ src/providers/moodLists.provider.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { MoodOptionType, MoodOptionWithTimestamp } from '../types';
+
+const MOOD_LIST_KEY = 'mood-list-key';
 
 interface MoodListContextType {
   moodList: MoodOptionWithTimestamp[];
@@ -12,14 +15,58 @@ const defaultValue: MoodListContextType = {
 };
 
 const MoodListContext = React.createContext<MoodListContextType>(defaultValue);
-
 export const useMoodListContext = () => React.useContext(MoodListContext);
+
+/**
+ * function to get saved moods in async storage
+ * @returns - saved moodLists
+ */
+const loadSavedMoods = async (): Promise<
+  MoodOptionWithTimestamp[] | undefined
+> => {
+  try {
+    const moods = await AsyncStorage.getItem(MOOD_LIST_KEY);
+    if (moods) {
+      return JSON.parse(moods) as MoodOptionWithTimestamp[];
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * function to persist current app moods to async storage
+ * @param moods - moods to be persisted
+ */
+const saveMoods = async (moods: MoodOptionWithTimestamp[]): Promise<void> => {
+  try {
+    await AsyncStorage.setItem(MOOD_LIST_KEY, JSON.stringify(moods));
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const MoodListProvider: React.FC = ({ children }) => {
   const [moodList, setMoodList] = React.useState<MoodOptionWithTimestamp[]>([]);
 
+  React.useEffect(() => {
+    const getSavedMoods = async () => {
+      const moods = await loadSavedMoods();
+      if (moods) {
+        setMoodList(moods);
+      }
+    };
+
+    getSavedMoods();
+  }, []);
+
   const handleSelectMood = React.useCallback((mood: MoodOptionType) => {
-    setMoodList(current => [...current, { mood, timestamp: Date.now() }]);
+    setMoodList(current => {
+      const updatedMoods = [...current, { mood, timestamp: Date.now() }];
+      saveMoods(updatedMoods);
+
+      return updatedMoods;
+    });
   }, []);
 
   return (
