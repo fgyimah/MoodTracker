4ff9719efd989fcb3f4b648e5b4e4e7cf3c9ae60
Francis Gyimah <fgyimah883@gmail.com>
2022-01-02 16:32:53 +0000

    feat: added fancy charts

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { VictoryPie } from 'victory-native';
+import R from 'ramda';
+import { useMoodListContext } from '../providers/moodLists.provider';
+import { theme } from '../theme';
 
 export const Analytics: React.FC = () => {
+  const { moodList } = useMoodListContext();
+
+  const data = Object.entries(
+    R.groupBy(({ mood }) => mood.emoji, moodList),
+  ).map(([key, value]) => ({ x: key, y: value.length }));
+
   return (
-    <View>
-      <Text>Analytics Screen</Text>
+    <View style={styles.container}>
+      <VictoryPie
+        labelRadius={80}
+        radius={150}
+        innerRadius={50}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+        style={{ labels: { fontSize: 30 } }}
+        data={data}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
