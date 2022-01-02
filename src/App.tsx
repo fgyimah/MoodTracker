065feaa48ce576eee45cec8d09b10ca070742feb
Francis Gyimah <fgyimah883@gmail.com>
2022-01-02 16:21:57 +0000

    feat: added splash screen for android

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import Toast from 'react-native-toast-message';
+import SplashScreen from 'react-native-splash-screen';
 import { MoodListProvider } from './providers/moodLists.provider';
 import { BottomTabsNavigator } from './screens/BottomTabs.navigator';
 
 export const App: React.FC = () => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <MoodListProvider>
       <NavigationContainer>
